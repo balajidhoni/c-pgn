ef0b92de47903958b06198eea28575efe6cb0f60
balajidhoni <35687870+balajidhoni@users.noreply.github.com>
2018-02-02 16:18:56 +0530

    Update bala.c

--- bala.c
+++ bala.c
@@ -6,15 +6,8 @@ int main()
 
     printf("Enter an alphabet: ");
     scanf("%c",&c);
-
-    // evaluates to 1 (true) if c is a lowercase vowel
     isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-
-    // evaluates to 1 (true) if c is an uppercase vowel
-    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-    // evaluates to 1 (true) if either isLowercaseVowel or isUppercaseVowel is true
-    if (isLowercaseVowel || isUppercaseVowel)
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'u');
         printf("%c is a vowel.", c);
     else
         printf("%c is a consonant.", c);
